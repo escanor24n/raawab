9697244a4bb166cf0e26d463a7d5a68b798552c1
escanor24n <67058759+escanor24n@users.noreply.github.com>
2020-06-19 01:50:53 +0200

    Add files via upload

---
+++ schachbrett.py
@@ -0,0 +1,16 @@
+# Das / steht für geteilt
+
+summe = 0
+
+for feld in range(64):
+    reiskorn = 2**feld
+    summe = summe + reiskorn
+    print("Feld {}. = {:>30,} Reiskörner und insgesamt \
+            {:>30,}Reiskörmer"\
+           .format(feld+1,reiskorn,summe))
+
+gewicht = summe * 0.02 / 1000 / 1000
+print()
+print("Wenn ein Reiskorn 0,02 Gramm wiegt,wiegen die gesamten"\
+      " Reiskörner {:,.0f} Tonnen".format(gewicht))
+
